bf4ecae7925992bd539c5f3c8ee2abdc8a1a69aa
Jamily <milyprotschnot@gmail.com>
2026-03-24 23:05:37 -0300

    Corrigindo automação v2

--- run.sh
+++ run.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# parar container se existir
-docker stop calculadora-api 2>/dev/null
-
-# remover container se existir
-docker rm calculadora-api 2>/dev/null
-
 # Build da imagem
 docker build -t calculadora-api -f jamily.Dockerfile .
 
